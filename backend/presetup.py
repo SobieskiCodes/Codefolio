4e08ccb5889e2f666f214fa3cfc2d24850d9b80d
JUSTIN SOBIESKI <jmksobieski@gmail.com>
2024-02-10 16:50:21 +0000

    Potential fix for env.

--- backend/presetup.py
+++ backend/presetup.py
@@ -1,17 +1,45 @@
+import subprocess
 import shutil
+import sys
 import os
 
-current_dir = os.path.dirname(os.path.realpath(__file__))
-parent_dir = os.path.dirname(current_dir)
+def main():
+    # Check if the correct number of arguments are provided
+    if len(sys.argv) != 3:
+        print("Usage: python presetup.py <CODESPACE_NAME> <GITHUB_TOKEN>")
+        sys.exit(1)
 
-# Define the source and destination file paths
-source_file = '/workspaces/.codespaces/shared/.env'
-destination_dir = f'{parent_dir}/frontend'
+    # Retrieve arguments
+    codespace_name = sys.argv[1]
+    github_token = sys.argv[2]
+    env_content = f"VITE_CODESPACE_NAME={codespace_name}\nVITE_GITHUB_TOKEN={github_token}\n"
 
-# Construct the destination file path
-destination_file = os.path.join(destination_dir, os.path.basename(source_file))
+    # Define the source and destination file paths
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    parent_dir = os.path.dirname(current_dir)
+    destination_dir = f'{parent_dir}/frontend'
 
-# Move the file
-shutil.move(source_file, destination_file)
+    # Construct the destination file path
+    env_file_path = os.path.join(destination_dir, '.env')
 
-print(f"Moved {source_file} to {destination_file}")
+    # Prepare the content to write to the .env file
+    env_content = f"VITE_CODESPACE_NAME={codespace_name}\nVITE_GITHUB_TOKEN={github_token}\n"
+
+    # Writing to the .env file
+    try:
+        with open(env_file_path, 'w') as env_file:
+            env_file.write(env_content)
+            print(f".env file created at {env_file_path}")
+    except Exception as e:
+        print(f"Failed to write to {env_file_path}: {e}")
+
+    # Set port visibility public
+    #set_port_visibility_public(codespace_name)
+
+def set_port_visibility_public(codespace_name):
+    # Call gh commands using subprocess
+    subprocess.run(["gh", "codespace", "ports", "visibility", "3000:public", "-c", codespace_name], check=True)
+    subprocess.run(["gh", "codespace", "ports", "visibility", "8000:public", "-c", codespace_name], check=True)
+
+if __name__ == "__main__":
+    main()
