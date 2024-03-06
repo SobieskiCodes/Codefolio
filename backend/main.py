ac09716bfd0e19c7f2b0d9db8a8607723f685cc3
Justin Sobieski <12957753+SobieskiCodes@users.noreply.github.com>
2024-03-05 20:34:27 -0500

    refactor(backend): remove duplicate import (pre-release)

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,6 @@ from app.db.database import SessionLocal, engine
 from app.db.models import store_model
 from app.api.endpoints.store_endpoint import router as store_router  
 import os
-from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 store_model.Base.metadata.create_all(bind=engine)
 from dotenv import load_dotenv
@@ -38,29 +37,27 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.include_router(store_router, prefix="/api/classwork/store")  # You can specify a prefix if desired
+app.include_router(store_router, prefix= "/api/classwork/store")  
 
 
-# @app.exception_handler(ValidationError)
-# async def validation_exception_handler(request: Request, exc: ValidationError):
-#     # Format the validation errors as you like
-#     errors = []
-#     for error in exc.errors():
-#         error_item = {
-#             "location": error['loc'],
-#             "message": error['msg'],
-#             "type": error['type']
-#         }
-#         errors.append(error_item)
+@app.exception_handler(ValidationError)
+async def validation_exception_handler(request: Request, exc: ValidationError):
+    errors = []
+    for error in exc.errors():
+        error_item = {
+            "location": error['loc'],
+            "message": error['msg'],
+            "type": error['type']
+        }
+        errors.append(error_item)
 
-#     # Return a JSON response with the error details and a 422 Unprocessable Entity status
-#     return JSONResponse(
-#         status_code=422,
-#         content={
-#             "detail": errors,
-#             "body": await request.json()  # Including the original body can be helpful
-#         },
-#     )
+    return JSONResponse(
+        status_code=422,
+        content={
+            "detail": errors,
+            "body": await request.json()  # Including the original body 
+        },
+    )
 
 @app.get("/")
 def read_root():
